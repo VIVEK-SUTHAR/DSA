714edba00b57b59aa4f1525cc242a0e606014a2a
Vivek Suthar <82870307+VIVEK-SUTHAR@users.noreply.github.com>
2022-02-17 16:21:36 +0530

    Quick Sort added

---
+++ Searching And Sorting/Sorting/Quick-sort.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+void swap(int *a, int *b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+int partition(int array[], int low, int high)
+{
+    int pivot = array[high];
+    int i = (low - 1);
+    for (int j = low; j < high; j++)
+    {
+        if (array[j] <= pivot)
+        {
+            i++;
+            swap(&array[i], &array[j]);
+        }
+    }
+    swap(&array[i + 1], &array[high]);
+    return (i + 1);
+}
+void quickSort(int array[], int low, int high)
+{
+    if (low < high)
+    {
+        int pi = partition(array, low, high);
+        quickSort(array, low, pi - 1);
+        quickSort(array, pi + 1, high);
+    }
+}
+void printArray(int array[], int size)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        cout << array[i] << " ";
+    }
+    cout << "\n";
+}
+int main()
+{
+    int size;
+    cout << "Enter Array size\n";
+    cin >> size;
+    int data[size];
+    cout << "Enter " << size << " Elements\n";
+    for (int i = 0; i < size; i++)
+    {
+        cin >> data[i];
+    }
+    int n = sizeof(data) / sizeof(data[0]);
+    cout << "Unsorted Array\n";
+    printArray(data, n);
+    quickSort(data, 0, n - 1);
+    cout << "Sorted Array\n";
+    printArray(data, n);
+}
